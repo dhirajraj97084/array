bbedb07803fe01106aeba18148529db7bb913eb4
Dhiraj-Tiwari <rajtiwari73011@gmail.com>
2024-08-29 21:36:39 +0530

    reverce

---
+++ reverceArray.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int arr[]={2,3,4,5,15,0,-6,77};
+     int n=sizeof(arr)/sizeof(int); 
+   
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    int i=0,j=n-1; 
+    while (i<j)
+    {
+        /* code */
+        swap(arr[i],arr[j]);
+        i++;
+        j--;
+    }
+     for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;   
+
+}
